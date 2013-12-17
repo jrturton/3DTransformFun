6d1937b07c0ef6c36163b2820631e54b32ec421b
richard.turton@mubaloo.com <richard.turton@windows-phone.moo.mubaloo.com>
2013-12-17 12:06:45 +0000

    Anchor point editing

--- 3DTransformFun/TDTViewController.m
+++ 3DTransformFun/TDTViewController.m
@@ -16,22 +16,30 @@
 
 @property (strong,nonatomic) TDTransform* currentTransform;
 
-#pragma mark - Outlets
-@property (strong, nonatomic) IBOutlet TDTTransformDemoView *transformView;
+#pragma mark - Child view controllers
 @property (strong, nonatomic) IBOutlet TDTTransformStackViewController *transformStackVC;
 @property (strong, nonatomic) IBOutlet TDTTransformEditorViewController *transformEditorVC;
-@property (weak, nonatomic) IBOutlet UIButton *typeButton;
-@property (weak, nonatomic) IBOutlet UITextField *nameField;
-@property (weak, nonatomic) UIPopoverController *popover;
 
-@property (weak, nonatomic) IBOutlet UIButton *animateButton;
+#pragma mark - Controls
+@property (strong, nonatomic) IBOutlet UIButton *typeButton;
+@property (strong, nonatomic) IBOutlet UITextField *nameField;
+@property (strong, nonatomic) IBOutlet UIButton *animateButton;
+@property (strong, nonatomic) IBOutlet UILabel *anchorPointXLabel;
+@property (strong, nonatomic) UIPopoverController *popover;
+@property (strong, nonatomic) IBOutlet UILabel *anchorPointYLabel;
+@property (strong, nonatomic) IBOutlet UILabel *anchorPointZLabel;
+- (IBAction)anchorChanged:(UIStepper *)sender;
+
+#pragma mark - Transform
+@property (strong, nonatomic) IBOutlet TDTTransformDemoView *transformView;
 @property (nonatomic) BOOL isAnimating;
+@property (strong, nonatomic) CAShapeLayer *anchorPointIndicator;
 
 @end
 
 @implementation TDTViewController
-- (IBAction)animateTapped:(UIButton *)sender {
-    self.isAnimating = !self.isAnimating;
+{
+    CGRect originalFrame;
 }
 
 - (void)viewDidLoad
@@ -41,13 +49,25 @@
     CATransform3D sublayerTransform = CATransform3DIdentity;
     sublayerTransform.m34 = 0.0005;
     self.view.layer.sublayerTransform = sublayerTransform;
+    CATransformLayer *transformLayer = [CATransformLayer layer];
+    transformLayer.sublayerTransform = sublayerTransform;
+    [self.transformView.layer addSublayer:transformLayer];
+    transformLayer.frame = self.transformView.bounds;
+    CAShapeLayer *anchorPointIndicator = [CAShapeLayer layer];
+    anchorPointIndicator.path = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(-5.0,-5.0,10.0,10.0)].CGPath;
+    anchorPointIndicator.fillColor = [UIColor redColor].CGColor;
+    [transformLayer addSublayer:anchorPointIndicator];
+    self.anchorPointIndicator = anchorPointIndicator;
+    [self updateAnchorPointLabels];
 }
 
-- (void)didReceiveMemoryWarning
+-(void)viewDidAppear:(BOOL)animated
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [super viewDidAppear:animated];
+    originalFrame = self.transformView.frame;
 }
+
+
 
 #pragma mark - Segues
 
@@ -163,5 +183,69 @@
     }
     _isAnimating = isAnimating;
     self.animateButton.selected = isAnimating;
+}
+
+#pragma mark - Actions
+
+- (IBAction)animateTapped:(UIButton *)sender
+{
+    self.isAnimating = !self.isAnimating;
+}
+- (IBAction)anchorChanged:(UIStepper *)sender
+{
+    CGFloat roundedValue;
+    if (sender.tag == 0 || sender.tag == 1)
+        roundedValue = 0.1 * roundf(10 * (CGFloat)sender.value);
+    else
+        roundedValue = (CGFloat)sender.value;
+
+    CGPoint anchorPoint = self.transformView.layer.anchorPoint;
+    CGFloat z = self.transformView.layer.anchorPointZ;
+
+    switch (sender.tag)
+    {
+        case 0:
+            anchorPoint.x = roundedValue;
+            break;
+        case 1:
+            anchorPoint.y = roundedValue;
+            break;
+        case 2:
+            z = roundedValue;
+            break;
+        default:
+            break;
+    }
+    [self setTransformAnchorPoint:anchorPoint z:z];
+}
+
+-(void)setTransformAnchorPoint:(CGPoint)anchorPoint z:(CGFloat)z
+{
+    self.isAnimating = NO;
+    [CATransaction begin];
+    [CATransaction setDisableActions:YES];
+    CATransform3D currentTransform = self.transformView.layer.transform;
+    self.transformView.layer.transform = CATransform3DIdentity;
+    self.transformView.layer.anchorPoint = anchorPoint;
+    self.transformView.layer.anchorPointZ = z;
+    self.transformView.frame = originalFrame;
+    self.transformView.layer.transform = currentTransform;
+    [CATransaction commit];
+    [self updateAnchorPointLabels];
+}
+
+-(void)updateAnchorPointLabels
+{
+    CGPoint anchorPoint = self.transformView.layer.anchorPoint;
+    CGFloat z = self.transformView.layer.anchorPointZ;
+
+    self.anchorPointXLabel.text = [NSString stringWithFormat:@"X: %.1f",anchorPoint.x];
+    self.anchorPointYLabel.text = [NSString stringWithFormat:@"Y: %.1f",anchorPoint.y];
+    self.anchorPointZLabel.text = [NSString stringWithFormat:@"Z: %.0f", z];
+
+    CGFloat edge = self.transformView.bounds.size.width;
+    self.anchorPointIndicator.position = CGPointMake(edge * anchorPoint.x, edge * anchorPoint.y);
+    //TODO: This isn't working
+    self.anchorPointIndicator.transform = CATransform3DMakeTranslation(0.0, 0.0, -z);
 }
 @end
